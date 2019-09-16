93a43fa9aa290c456233f4298a33752c191a5a5f
Marie Walker <LourdesMarieWalker@gmail.com>
2019-09-15 17:33:23 -0700

    Updates

---
+++ precache-manifest.a1e51d2e3e0147d733acf5a644ce0a98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f70fff7412d8a7fa35dda129cd0b0ed",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "425ee1456b3ce259f545",
+    "url": "/monsters-rolodex/static/css/main.9266e3b5.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monsters-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "425ee1456b3ce259f545",
+    "url": "/monsters-rolodex/static/js/main.7a81ca3b.chunk.js"
+  },
+  {
+    "revision": "5f535753dcec63ef3476",
+    "url": "/monsters-rolodex/static/js/runtime~main.7352cc78.js"
+  }
+]);
